9946a9623fa214243f7ee18ce4449d51acd8f85c
李言 <liyan@liyandeMacBook-Pro.local>
2014-08-07 18:16:00 +0800

    Initial Commit

---
+++ ViewControllerTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewControllerTest
+//
+//  Created by 李言 on 14-8-7.
+//  Copyright (c) 2014年 ___李言___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewControllerTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewControllerTest
+//
+//  Created by 李言 on 14-8-7.
+//  Copyright (c) 2014年 ___李言___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
